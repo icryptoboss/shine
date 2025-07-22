8785a682b662c2fc169c0191065e23130ceb77da
icryptoboss <141033841+icryptoboss@users.noreply.github.com>
2025-07-22 13:36:36 +0530

    Update saini.py

--- saini.py
+++ saini.py
@@ -292,16 +292,15 @@ async def download_and_decrypt_video(url, cmd, name, key):
             print(f"Failed to decrypt {video_path}.")  
             return None  
 
-async def send_vid(bot: Client, m: Message,cc,filename,thumb,name,prog):
+async def send_vid(bot: Client, m: Message,cc,filename,thumb,name,prog,channel_id):
     subprocess.run(f'ffmpeg -i "{filename}" -ss 00:00:10 -vframes 1 "{filename}.jpg"', shell=True)
     await prog.delete (True)
-    reply = await m.reply_text(f"<b>Generate Thumbnail:</b>\n<blockquote><b>{name}</b></blockquote>")
+    reply = await m.reply_text(f"<b>Uploading:</b>\n<blockquote><b>{name}</b></blockquote>")
     try:
         if thumb == "/d":
             thumbnail = f"{filename}.jpg"
         else:
             thumbnail = thumb
-            
     except Exception as e:
         await m.reply_text(str(e))
       
@@ -309,11 +308,22 @@ async def send_vid(bot: Client, m: Message,cc,filename,thumb,name,prog):
     start_time = time.time()
 
     try:
-        await m.reply_video(filename,caption=cc, supports_streaming=True,height=720,width=1280,thumb=thumbnail,duration=dur, progress=progress_bar,progress_args=(reply,start_time))
+        await bot.send_video(
+            chat_id=channel_id,
+            video=filename,
+            caption=cc, 
+            supports_streaming=True,
+            height=720,
+            width=1280,
+            thumb=thumbnail,
+            duration=dur, 
+            progress=progress_bar,
+            progress_args=(reply,start_time))
     except Exception:
-        await m.reply_document(filename,caption=cc, progress=progress_bar,progress_args=(reply,start_time))
+        await bot.send_document(chat_id=channel_id, document=filename,caption=cc, progress=progress_bar,progress_args=(reply,start_time))
     
     finally:
         await reply.delete(True)
         os.remove(filename)
-        os.remove(f"{filename}.jpg")
+        if os.path.exists(f"{filename}.jpg"):
+            os.remove(f"{filename}.jpg")
